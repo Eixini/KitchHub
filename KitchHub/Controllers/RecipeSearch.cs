b26c3ce644453c20fec3ce5f8f56626570ef164b
Eixini <eixi16@gmail.com>
2023-02-02 13:26:55 +0300

    Add RecipeSearch controller
    Added method for receiving and processing data from Frontend

---
+++ KitchHub/Controllers/RecipeSearch.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.JSInterop;
+using System.Dynamic;
+
+namespace KitchHub.Controllers;
+
+public class RecipeSearch : Controller
+{
+	private readonly ILogger<RecipeSearch> _logger;
+
+	public RecipeSearch(ILogger<RecipeSearch> logger)
+	{
+		_logger = logger;
+	}
+
+	public IActionResult EnterIngredientsPage()
+    {
+        return View();
+    }
+
+	/// <summary>
+	/// Метод для получения ингредиентов из "поля тегов" и поиск рецептов по имеющимся тегам.
+	/// Ингредиенты получаются из представления в виде массива строк,
+	/// после чего, происходит сравнение введенных ингредиентов с ингредиентами рецептов из БД
+	/// </summary>
+	/// <param name="ingredients"> Введенные игредиенты из "поля тегов"</param>
+	[HttpGet]
+	public JsonResult SearchRecipe([FromQuery]string ingredients)
+	{
+		// Создание массива 
+		var ingParts = ingredients.Split(',');
+		foreach (string ing in ingParts)
+			Console.WriteLine(ing);
+
+		// ...
+		dynamic test = new ExpandoObject();
+		test.one = "It's test result";
+		test.two = 74;
+		test.three = new [] { 1,2,3,4,5 };
+
+		return Json(test);
+	}
+
+	public IActionResult ShowRecipes()
+	{
+		return View();
+	}
+}
