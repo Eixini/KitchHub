d1516f595233ddbb5ff900e5a3a63ad79b294181
Eixini <eixi16@gmail.com>
2023-03-15 01:26:30 +0300

    Delete former implementation

--- KitchHub/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace KitchHub.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
