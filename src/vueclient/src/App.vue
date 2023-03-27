607afba7ff6f5a10298457efd00f3be50781d20b
Eixini <eixi16@gmail.com>
2023-03-28 02:40:25 +0300

    Fix style

--- src/vueclient/src/App.vue
+++ src/vueclient/src/App.vue
@@ -1,11 +1,29 @@
 <template>
   <div id="app">
-    <div>
-      <router-link to="/">Home</router-link>
-      <router-link to="/enteringingredients">Recipe Search</router-link>
+    <div class="header">
+      <div class="container">
+        <div class="nav">
+          KitchHub
+        </div>
+        <div class="navbar">
+          <div>
+            <router-link to="/">Home</router-link>
+          </div>
+          <div>
+            <router-link to="/enteringingredients">Recipe Search</router-link>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="content">
-      <router-view></router-view>
+    <div class="main">
+      <div class="container">
+        <div class="content">
+
+        </div>
+        <div class="content-center">
+          <router-view></router-view>
+        </div>
+      </div>
     </div>
     
   </div>
@@ -23,11 +41,51 @@ export default {
 <style>
 
   #app {
-    width: 100%;
+    margin: 0;
+    box-sizing: border-box;
   }
 
-  .content{
+  .container{
     max-width: 800px;
+    padding: 10px;
     margin: 0 auto;
   }
+
+  .content-center {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .header {
+    background-color: black;
+  }
+
+  .header .container {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .navbar {
+    display: flex;
+  }
+
+  .navbar div {
+    margin-right: 10px;
+  }
+
+  a {
+    /* Стили для router-link */
+    color: white;
+    text-decoration: none;
+  }
+
+  .nav {
+    color: white
+  }
+
+  .navbar div:last-child {
+    margin-right: 0;
+  }
+
 </style>
