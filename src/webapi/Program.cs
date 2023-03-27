7b14cda34ed4afe59c46d87f229efea8a02dcd54
Eixini <eixi16@gmail.com>
2023-03-28 01:00:59 +0300

    Add comment

--- src/webapi/Program.cs
+++ src/webapi/Program.cs
@@ -8,8 +8,6 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
-
         builder.Services.AddControllers();
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddCors();
