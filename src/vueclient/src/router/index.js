323a97e315c934e2dc1ee18cce28711db9566703
Eixini <eixi16@gmail.com>
2023-04-06 15:44:22 +0300

    Add routes for reg, auth and profile

--- src/vueclient/src/router/index.js
+++ src/vueclient/src/router/index.js
@@ -3,12 +3,18 @@ import { createRouter } from 'vue-router'
 import Home from '@/components/Home.vue'
 import EnteringIngredients from '@/components/EnteringIngredients.vue'
 import ShowResultRecipes from '@/components/ShowResultRecipes.vue'
+import Profile from '@/components/Profile.vue'
+import Registration from '@/components/Registration.vue'
+import Authorization from '@/components/Authorization.vue'
 
 
 const routes = [
     { path: '/', component: Home },
     { path: '/enteringingredients', component: EnteringIngredients },
-    { path: '/showresultrecipes', component: ShowResultRecipes }
+    { path: '/showresultrecipes', component: ShowResultRecipes },
+    { path: '/login', component: Authorization },
+    { path: '/register', component: Registration },
+    { path: '/profile', component: Profile }
 ]
 
 export default function(history) {
