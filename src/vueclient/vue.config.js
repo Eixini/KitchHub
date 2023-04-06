569942221950ff9457bb08a38683b88c6e6c2867
Eixini <eixi16@gmail.com>
2023-04-06 15:44:09 +0300

    Fix path

--- src/vueclient/vue.config.js
+++ src/vueclient/vue.config.js
@@ -24,7 +24,7 @@ module.exports = {
             cert: fs.readFileSync(certFilePath),
         },
         proxy: {
-            '^/': {
+            '/': {
                 target: 'https://localhost:5192/'
             }
         },
