cb1c5e71cc10c3c491706987a82c5ee09082c119
Eixini <eixi16@gmail.com>
2023-04-06 15:45:03 +0300

    Add Authentication

--- src/webapi/Program.cs
+++ src/webapi/Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Configuration;
+using Microsoft.Extensions.Configuration;
 
 namespace webapi;
 
@@ -6,16 +11,44 @@ public class Program
 {
     public static void Main(string[] args)
     {
+
         var builder = WebApplication.CreateBuilder(args);
+
+        var tokenKey = builder.Configuration.GetValue<string>("TokenKey");
+        var key = Encoding.ASCII.GetBytes(tokenKey);
 
         builder.Services.AddControllers();
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddCors();
         builder.Services.AddSwaggerGen();
-        builder.Services.AddDbContext<KitchHubDbContext>(options =>
+        builder.Services.AddDbContext<KitchHubDbContext> (options =>
         {
             options.UseSqlite(@"Data Source=KitchHub.db;Cache=Shared;");
+
         });
+
+        builder.Services.AddDbContext<UsersKitchHubDbContext>(options =>
+        {
+            options.UseSqlite(@"Data Source=UsersKitchHub.db;Cache=Shared;");
+        });
+
+        builder.Services.AddAuthentication(conf =>
+        {
+            conf.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            conf.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(x =>
+        {
+            x.RequireHttpsMetadata = false;
+            x.SaveToken = true;
+            x.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = false,
+                ValidateAudience = false
+            };
+        });
+        builder.Services.AddSingleton<IJWTAuthenticationManager>(new JWTAuthenticationManager(tokenKey));
 
         var app = builder.Build();
 
@@ -36,6 +69,7 @@ public class Program
 
         app.UseHttpsRedirection();
 
+        app.UseAuthentication();
         app.UseAuthorization();
 
 
