c7d4f069eee7dbfc00dea4165d5dc9c6534930d4
Eixini <eixi16@gmail.com>
2023-04-14 01:49:19 +0300

    Add methods

--- src/vueclient/src/components/Profile.vue
+++ src/vueclient/src/components/Profile.vue
@@ -1,11 +1,23 @@
 <template>
     <div class="content">
         <h3>User Profile</h3>
-        
+        <div>
+            <span> {{ currentUser.nickname }}</span>
+        </div>
     </div>
 </template>
 
 <script lang="js">
+
+export default {
+
+    computed: {
+    currentUser() {
+      return this.$store.state.auth.user;
+    }
+  },
+
+}
 
 </script>
 
