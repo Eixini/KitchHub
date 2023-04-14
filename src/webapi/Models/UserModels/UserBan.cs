bf756eab58a569ffcdc2dbd33034492fa2a1efd1
Eixini <eixi16@gmail.com>
2023-04-15 01:43:04 +0300

    Add entity for banlist

---
+++ src/webapi/Models/UserModels/UserBan.cs
@@ -0,0 +1,10 @@
+﻿namespace webapi;
+
+public class UserBan
+{
+    public DateTime BanTime { get; set; }
+    public DateTime BanDuration { get; set; }
+    public string BanCause { get; set; } = null!;
+    public User WhoBanned { get; set; } = null!;
+
+}
