cf6fd894328c6e81646eb695ea791ee4bc7f496d
Eixini <eixi16@gmail.com>
2023-04-16 03:55:25 +0300

    Add moderator panel

--- src/vueclient/src/components/privilege/ModerPanel.vue
+++ src/vueclient/src/components/privilege/ModerPanel.vue
@@ -1,11 +1,31 @@
 <template>
     <div class="content">
-
+        <div class="moder-tabs">
+            <button type="button" @click="component='RecipesForModeration'">Рецепты для проверки</button>
+            <button type="button" @click="component='ViolationReports'">Сообщения о нарушении</button>
+        </div>
+        <div class="moder-content">
+            <keep-alive>
+                <component :is="component"></component>
+            </keep-alive>
+        </div>
     </div>
 </template>
 
 <script lang="js">
+import RecipesForModeration from './partial/RecipesForModeration.vue';
+import ViolationReports from './partial/ViolationReports.vue';
+
 export default {
+    components:{
+        RecipesForModeration,
+        ViolationReports,
+    },
+    data(){
+        return {
+            component: 'RecipesForModeration',
+        }
+    }
     
 }
 </script>
