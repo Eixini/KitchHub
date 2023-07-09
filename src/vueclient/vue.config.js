086624fef071c75723d8738ddba55afcfc464b1a
Eixini <eixi16@gmail.com>
2023-07-09 13:02:52 +0300

    Change settings

--- src/vueclient/vue.config.js
+++ src/vueclient/vue.config.js
@@ -5,8 +5,8 @@ module.exports = defineConfig({
   transpileDependencies: true,
 
   publicPath: process.env.NODE_ENV === 'production'
-  ? '/production-sub-path/'
-  : '/',
+  ? '/'
+  : '/dev/',
 
   devServer: {
     https: {
